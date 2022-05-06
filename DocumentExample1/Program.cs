c5c6e8b6cf7019a2804818e59ed522d73ab5f164
PTRL481\Anusha.Medichelmi <anusha.m@xcubelabs.com>
2022-05-06 10:26:11 +0530

    Example of C sharp language

---
+++ DocumentExample1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocumentExample1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.WriteLine("My Name is Anusha Medichelmi");
+            Console.WriteLine("I'm an intern at PurpleTalk");
+            Console.WriteLine("Bye");
+            Console.WriteLine();
+        }
+    }
+}
